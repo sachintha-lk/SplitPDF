423b7aca8f44921dfee2177e74252b4440af7388
sachintha <sachintha.l.k@hotmail.com>
2021-11-07 13:31:01 +0530

    First Commit

---
+++ SplitPdf1.0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+try:
+    from PyPDF2 import PdfFileReader, PdfFileMerger,PdfFileWriter
+    import webbrowser, time
+except Exception as e1:
+    print("Unable to import modules or ", e1)
+
+
+def main():
+    print("PDF Spliter for Canon LBP6030 Duplex print".center(50, " "))
+    print("Beta edition".center(50, " "))
+    print("Print order \n 1) pdfSplit1.pdf \n 2) pdfSplit2.pdf")
