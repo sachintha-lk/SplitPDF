bc78366f1a8412303ccc76d3aad1a4029c45e76e
sachintha <sachintha.l.k@hotmail.com>
2021-11-07 13:39:19 +0530

    patch 1 idk

--- SplitPdf1.0.py
+++ SplitPdf1.0.py
@@ -5,8 +5,12 @@ try:
 except Exception as e1:
     print("Unable to import modules or ", e1)
 
+class pdfFile:
+    def self():
+        self.
 
 def main():
     print("PDF Spliter for Canon LBP6030 Duplex print".center(50, " "))
     print("Beta edition".center(50, " "))
     print("Print order \n 1) pdfSplit1.pdf \n 2) pdfSplit2.pdf")
+
